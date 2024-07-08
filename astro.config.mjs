e54e1ec4fe83b0e15d6866262f1c13e2a1bdd4ea
ikrarun <iamkrarun@gmail.com>
2024-07-08 22:35:33 +0530

    Changed Layout Approach

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
 
-import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    domains: ["picsum.photos", "images.unsplash.com", "images.pexels.com"]
-  },
-  integrations: [tailwind(), mdx()]
+  integrations: [icon(), tailwind()]
 });
