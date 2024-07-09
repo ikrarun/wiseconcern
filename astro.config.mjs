117ae1b6c65115c556af727819ac767de59bdf1e
ikrarun <iamkrarun@gmail.com>
2024-07-10 01:23:31 +0530

    added service worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
-
+import serviceWorker from "astrojs-service-worker";
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), tailwind(), react(), svelte(), partytown()],
+  integrations: [icon(), tailwind(), react(), svelte(), partytown(), serviceWorker({
+    workbox: { inlineWorkboxRuntime: true }
+  })],
   image: {
     domains: ['https://images.pexels.com/']
   }
