349bbe73691bd7d8e3ee40d2acd5ff8cfd97826d
ikrarun <iamkrarun@gmail.com>
2024-07-24 22:42:05 +0530

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/serverless";
-
+import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), tailwind(), react(), svelte()],
+  build: {
+    format: "file",
+  },
+  integrations: [icon(), tailwind(), react(), svelte(),pagefind()],
   image: {
     domains: ['https://images.pexels.com/','https://images.unsplash.com/']
   },
