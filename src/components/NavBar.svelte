083aad2d4263a916bd94d73d8740671d9964e47c
ikrarun <iamkrarun@gmail.com>
2024-07-24 18:49:03 +0530

    update NavBar.svelte, src/pages/about/index.astro, 404.astro and [id].astro

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -10,15 +10,12 @@
     isSearchOpen = !isSearchOpen;
   };
   const handleFormSubmission = (e: Event) => {
+    console.log("triggered");
     const formData = new FormData(e.target as HTMLFormElement);
     console.log(formData.get("search"));
     window.location.href = `/blogs/${formData.get("search")}`;
   };
 
-  window.addEventListener("resize", () => {
-    isSearchOpen = false;
-    isMenuOpen = false;
-  });
   const toggleMenuOpen = () => {
     isMenuOpen = !isMenuOpen;
   };
@@ -36,9 +33,9 @@
   class="w-full flex fixed transition-all bg-amber-400 text-black duration-100 ease-in-out left-0 right-0 top-0 z-[500] items-center mx-auto justify-center"
 >
   <div
-    class="inline-flex justify-between max-w-[900px] px-4 items-center w-full py-2"
+    class="inline-flex z-[500] justify-between max-w-[900px] px-4 items-center w-full py-2"
   >
-    <button class="" on:click|preventDefault={toggleMenuOpen}>
+    <button class="z-[500]" on:click|preventDefault={toggleMenuOpen}>
       {#if isMenuOpen}
         <Icon icon="mdi:close" class="text-xl" />
       {:else}
@@ -46,10 +43,10 @@
       {/if}</button
     >
 
-    <a href="/" class="font-black text-3xl uppercase">{title}</a>
+    <a href="/" class="font-black z-[500] text-3xl uppercase">{title}</a>
 
-    <button on:click|preventDefault={toggleIsSearchOpen}
-      ><Icon icon="mdi:magnify" class="text-xl" /></button
+    <button on:click|preventDefault={toggleIsSearchOpen} class="z-[500]"
+      ><Icon icon="mdi:magnify" class="text-xl z-[500]" /></button
     >
   </div>
 
@@ -57,21 +54,21 @@
   <div
     class="{isSearchOpen
       ? 'flex'
-      : 'hidden'} flex-col z-[500] gap-4 p-8 justify-center items-center w-full h-fit bg-black fixed top-0 left-0 right-0 transition-all duration-300 ease-in-out"
+      : 'hidden'} flex-col z-[900] gap-4 p-8 justify-center items-center w-full h-fit bg-black fixed top-0 left-0 right-0 transition-all duration-300 ease-in-out"
   >
     <form
       on:submit|preventDefault={handleFormSubmission}
-      class="w-full max-w-[900px] z-[500] inline-flex items-center outline border-2 border-white rounded-full px-8 py-3 justify-center"
+      class="w-full max-w-[900px] z-[900] inline-flex items-center outline border-2 border-white rounded-full px-8 py-3 justify-center"
     >
       <input
         type="text"
         name="search"
         placeholder="What are you looking for?"
-        class="grow bg-transparent outline-none ring-0 text-white"
+        class="grow bg-transparent outline-none ring-0 z-[900] text-white"
       />
       <button
         type="submit"
-        class="inline-flex items-center justify-center gap-2 text-white"
+        class="inline-flex items-center justify-center z-[900] gap-2 text-white"
       >
         <Icon icon="mdi:magnify" class="text-xl" />
         <span>Search</span>
@@ -79,7 +76,7 @@
     </form>
     <button
       on:click|preventDefault={toggleIsSearchOpen}
-      class="inline-flex items-center justify-center z-[500] gap-2 text-white"
+      class="inline-flex items-center bg-white rounded-full px-4 py-2 text-black justify-center z-[900] gap-2"
     >
       <Icon icon="mdi:close" class="text-xl" />
       <span>Close</span>
@@ -93,7 +90,7 @@
       : '-top-full'} px-8 h-full bg-amber-400 text-black transition-all duration-700 ease-in-out"
   >
     <div
-      class="flex flex-col w-full  max-w-[380px] mx-auto justify-center items-center"
+      class="flex flex-col w-full max-w-[380px] mx-auto justify-center items-center"
     >
       <a href="/" class="text-xl">Home</a>
       <span class="rounded-full my-2 bg-black/30 w-2/3 h-0.5" />
@@ -104,12 +101,3 @@
     </div>
   </div>
 </div>
-
-<style>
-  @import url("https://fonts.googleapis.com/css2?family=Gabarito:wght@400..900&display=swap");
-  div {
-    font-family: "Gabarito", sans-serif;
-    font-optical-sizing: auto;
-    font-style: normal;
-  }
-</style>
