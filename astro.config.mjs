6d4bfa7ade4d0a3938e20e74bb42c1c9c1517aa4
ikrarun <iamkrarun@gmail.com>
2024-07-24 13:31:55 +0530

    theme-color change

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,11 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 import partytown from "@astrojs/partytown";
-
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), tailwind(), react(), svelte(), partytown()],
+  integrations: [icon(), tailwind(), react(), svelte(), partytown(),],
   image: {
     domains: ['https://images.pexels.com/']
   },
