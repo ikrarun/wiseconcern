c9ddcb16aed43368691fdef9d4a3506a2f3e195e
ikrarun <iamkrarun@gmail.com>
2024-07-25 21:15:19 +0530

    update astro.config.mjs and package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/serverless";
+import remarkEleventyImage from "astro-remark-eleventy-image";
+
 // https://astro.build/config
 export default defineConfig({
-
-  integrations: [icon(), tailwind(), react(), svelte(),],
+  integrations: [icon(), tailwind(), react(), svelte(), remarkEleventyImage()],
   image: {
     domains: ['https://images.pexels.com/', 'https://images.unsplash.com/']
   },
